0b7d43c5faca0833894189c8d376c3717c00d9f4
Lennart Popma <lennartpopma@hotmail.com>
2024-02-26 11:48:14 +0100

    Affiliate link logic improvement regarding brackets

--- src/app/data/affiliate-link.service.ts
+++ src/app/data/affiliate-link.service.ts
@@ -9,16 +9,10 @@ export class AffiliateLinkService {
     'bylashbabe': 'https://bylashbabe.com?sca_ref=3638091.TQRuyJNRG1',
     'myproteinnl': 'https://www.stylink.it/L3q2QUyG0l',
     'veromoda': 'https://www.stylink.it/AdZbXhpwmw',
-    'bjornborg': 'https://www.stylink.it/e7pw4T79X9',
+    'ginatricot': 'https://www.stylink.it/a51wOfppooL',
     'hunkemoller': 'https://www.stylink.it/7oZm3T2Y0n',
     'yesstyle': 'https://www.yesstyle.com/en/home.html?rco=YESGIRL23&utm_term=YESGIRL23&utm_medium=Influencer&utm_source=dynamic&mcg=influencer',
     'asos': 'https://www.stylink.it/lg3mpUa423',
-    'asos (outlet)': 'https://www.stylink.it/lg3mpUa423',
-    'asos (sale)': 'https://www.stylink.it/lg3mpUa423',
-    'asos (shoes)': 'https://www.stylink.it/lg3mpUa423',
-    'asos (sport)': 'https://www.stylink.it/lg3mpUa423',
-    'asos (via app)': 'https://www.stylink.it/lg3mpUa423',
-    'asos (jurkjes)': 'https://www.stylink.it/lg3mpUa423',
     'stevemaddeneu': 'https://www.stylink.it/AdZbXh7gYM',
     'lookfantastic': 'https://www.stylink.it/b46wYiyYx3',
     'hm': 'https://www.stylink.it/jnNwDTowPY',
@@ -37,8 +31,6 @@ export class AffiliateLinkService {
     'reginajewelry': 'https://www.reginajewelry.co/?ref=bb-jezj8p',
     'only': 'https://www.stylink.it/5xZndfMoYe',
     'nike': 'https://www.stylink.it/vR0jnhYnlR',
-    'nike (bh&leggings)': 'https://www.stylink.it/vR0jnhYnlR',
-    'nike (fleece)': 'https://www.stylink.it/vR0jnhYnlR',
     'prozis': 'https://prozis.com/NfHG',
     'stylevana': 'https://vana.ly/KmZChm',
     'temu': 'https://temu.to/m/u0p32ew176i',
@@ -48,6 +40,11 @@ export class AffiliateLinkService {
   constructor() { }
 
   public getAffiliateLink(company: string): string | undefined {
+    if(company.includes('(')) {
+      var parts = company.split('(');
+      company = parts[0].trim();
+    }
+
     return this.affiliateLinks[company];
   }
 }
